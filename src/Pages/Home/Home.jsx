370bf7e4383057437d2febb4556eec3500edbe5a
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-04-20 22:20:08 +0600

    initial project setup with tailwind,daisyUI and route defined with react router

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Banner from '../../Components/Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+        </div>
+    );
+};
+
+export default Home;
